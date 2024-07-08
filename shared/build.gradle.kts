d224a147049849c616362085aef13256592bcfc9
Evgenii Dobrovidov <mr.nate.river@gmail.com>
2024-07-08 20:35:31 +0200

    chore: move api typespec to a gradle subproject

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,16 +1,10 @@
-import com.bmuschko.gradle.docker.tasks.container.DockerCreateContainer
-import com.bmuschko.gradle.docker.tasks.container.DockerStartContainer
-import com.bmuschko.gradle.docker.tasks.image.DockerBuildImage
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-import org.openapitools.generator.gradle.plugin.tasks.GenerateTask as OpenApiGenerateTask
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.composeCompiler)
-    alias(libs.plugins.openapi)
-    alias(libs.plugins.docker)
 }
 
 kotlin {
@@ -45,76 +39,8 @@ kotlin {
     }
 }
 
-val rootPackage = "io.mrnateriver.smsproxy.shared"
-val proxyApiSpecDir = layout.projectDirectory.dir("src/commonMain/typespec/proxy-api")
-val proxyApiSpecPath = proxyApiSpecDir.file("tsp-output/@typespec/openapi3/openapi.yaml")
-val proxyApiOutputDirBase = layout.buildDirectory.dir("generated")
-
-val buildTypeSpecApiGenImageName = "buildTypeSpecApiGenImage"
-val typeSpecApiGenContainerTag = "sms-proxy-api-gen:latest"
-tasks.register<DockerBuildImage>(buildTypeSpecApiGenImageName) {
-    inputDir = proxyApiSpecDir
-    images.add(typeSpecApiGenContainerTag)
-}
-
-val createTypeSpecApiGenContainerName = "createTypeSpecApiGenContainer"
-tasks.register<DockerCreateContainer>(createTypeSpecApiGenContainerName) {
-    dependsOn(buildTypeSpecApiGenImageName)
-
-    imageId = typeSpecApiGenContainerTag
-    workingDir = "/app"
-    hostConfig.run {
-        autoRemove = true
-        binds.put("$proxyApiSpecDir/tsp-output", "/app/tsp-output")
-    }
-}
-
-tasks.register<DockerStartContainer>("startTypeSpecApiGenContainer") {
-    dependsOn(createTypeSpecApiGenContainerName)
-
-    containerId =
-        tasks.getByName<DockerCreateContainer>(createTypeSpecApiGenContainerName).containerId
-}
-
-apply(from = "src/commonMain/typespec/proxy-api/build.gradle.kts")
-
-// TODO: this generates buildable modules; we need either to incorporate those modules into the whole repo, or refactor the codegen to generate source files directly
-
-fun OpenApiGenerateTask.configureCommon(outputDirSuffix: String) {
-    inputSpec = proxyApiSpecPath.asFile.absolutePath
-
-    outputDir = "${proxyApiOutputDirBase}/$outputDirSuffix"
-    packageName = rootPackage
-    apiPackage = "$rootPackage.api"
-    modelPackage = "$rootPackage.models"
-
-    configOptions = mapOf("dateLibrary" to "kotlinx-datetime", "interfaceOnly" to "true")
-    skipValidateSpec = false
-    logToStderr = true
-    generateAliasAsModel = false
-    enablePostProcessFile = false
-}
-
-tasks.register<OpenApiGenerateTask>("generateApiServer") {
-    generatorName = "kotlin-server"
-    library = "ktor"
-
-    configureCommon("api-server")
-}
-
-tasks.register<OpenApiGenerateTask>("generateApiClient") {
-    generatorName = "kotlin"
-    library = "jvm-retrofit2"
-
-    configureCommon("api-client")
-}
-
-tasks.register("generateApi") {
-    dependsOn("generateApiServer", "generateApiClient")
-}
-
 android {
-    namespace = rootPackage
+    namespace = "io.mrnateriver.smsproxy.shared"
     compileSdk = libs.versions.android.compileSdk.get().toInt()
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
