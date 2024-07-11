cf77580fbbc5a3b472c58d0956a76d5e5e9aed79
Evgenii Dobrovidov <mr.nate.river@gmail.com>
2024-07-11 21:11:37 +0200

    feat: basic shared implementation of observability service for Android

---
+++ shared/src/androidMain/kotlin/io/mrnateriver/smsproxy/shared/AndroidObservabilityService.kt
@@ -0,0 +1,32 @@
+package io.mrnateriver.smsproxy.shared
+
+import android.util.Log
+import java.util.logging.Level
+
+// TODO: real observability, for instance Sentry
+class AndroidObservabilityService : ObservabilityService {
+    override fun log(level: Level, message: String) {
+        Log.println(mapLogLevel(level), "shared", message)
+    }
+
+    override suspend fun <T> runSpan(name: String, body: suspend () -> T): T {
+        log(Level.FINE, "Starting span: $name")
+        val result = body()
+        log(Level.FINE, "Ending span: $name")
+        return result
+    }
+
+    private fun mapLogLevel(level: Level): Int {
+        return when (level) {
+            Level.SEVERE -> Log.ERROR
+            Level.WARNING -> Log.WARN
+            Level.INFO -> Log.INFO
+            Level.CONFIG -> Log.DEBUG
+            Level.FINE -> Log.VERBOSE
+            Level.FINER -> Log.VERBOSE
+            Level.FINEST -> Log.VERBOSE
+            else -> Log.VERBOSE
+        }
+    }
+
+}
