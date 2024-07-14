8a214e106c2af18d5e09c24a0f052ea211a710c4
Evgenii Dobrovidov <mr.nate.river@gmail.com>
2024-07-14 13:27:56 +0200

    fix: add PARTIAL_WAKE_LOCK in relayApp

--- relayApp/src/androidMain/kotlin/io/mrnateriver/smsproxy/relay/MainApplication.kt
+++ relayApp/src/androidMain/kotlin/io/mrnateriver/smsproxy/relay/MainApplication.kt
@@ -1,8 +1,28 @@
 package io.mrnateriver.smsproxy.relay
 
+import android.annotation.SuppressLint
 import android.app.Application
+import android.content.Context
+import android.os.PowerManager
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class MainApplication : Application() {
+    private lateinit var wakeLock: PowerManager.WakeLock
+
+    @SuppressLint("WakelockTimeout")
+    override fun onCreate() {
+        super.onCreate()
+
+        wakeLock = (getSystemService(Context.POWER_SERVICE) as PowerManager).run {
+            newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "MainApplication::WakelockTag").apply {
+                acquire()
+            }
+        }
+    }
+
+    override fun onTerminate() {
+        super.onTerminate()
+        wakeLock.release()
+    }
 }
