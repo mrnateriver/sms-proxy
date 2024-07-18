fea758f78b39aa7fb7915f0472cfe577fb0436bd
Evgenii Dobrovidov <mr.nate.river@gmail.com>
2024-07-18 21:47:43 +0200

    feat: Room SQL debugging in relayApp

--- relayApp/src/androidMain/kotlin/io/mrnateriver/smsproxy/relay/services/data/MessagesDatabaseModule.kt
+++ relayApp/src/androidMain/kotlin/io/mrnateriver/smsproxy/relay/services/data/MessagesDatabaseModule.kt
@@ -7,13 +7,32 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import io.mrnateriver.smsproxy.relay.BuildConfig
+import io.mrnateriver.smsproxy.shared.contracts.ObservabilityService
+import java.util.concurrent.Executors
+import java.util.logging.Level
 
 @Module
 @InstallIn(SingletonComponent::class)
 class MessagesDatabaseModule {
     @Provides
-    fun providesMessagesDatabase(@ApplicationContext context: Context): MessagesDatabase =
-        Room.databaseBuilder(context, MessagesDatabase::class.java, "messages").build()
+    fun providesMessagesDatabase(
+        @ApplicationContext context: Context,
+        observabilityService: ObservabilityService,
+    ): MessagesDatabase {
+        val builder = Room.databaseBuilder(context, MessagesDatabase::class.java, "messages")
+
+        if (BuildConfig.DEBUG) {
+            builder.setQueryCallback({ sqlQuery, bindArgs ->
+                observabilityService.log(
+                    Level.FINEST,
+                    "SQL: $sqlQuery${if (bindArgs.isNotEmpty()) "\nArgs: $bindArgs" else ""}"
+                )
+            }, Executors.newSingleThreadExecutor())
+        }
+
+        return builder.build()
+    }
 
     @Provides
     fun providesMessagesDao(database: MessagesDatabase): MessagesDao = database.messagesDao()
