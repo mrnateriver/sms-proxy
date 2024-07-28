63ce21844bffe4d9da720cb874855f803ead40fd
Evgenii Dobrovidov <mr.nate.river@gmail.com>
2024-07-28 21:28:43 +0200

    fix: proper timeout for receiver key setting when relaying messages

--- relayApp/src/androidMain/kotlin/io/mrnateriver/smsproxy/relay/services/MessageRelayService.kt
+++ relayApp/src/androidMain/kotlin/io/mrnateriver/smsproxy/relay/services/MessageRelayService.kt
@@ -7,11 +7,13 @@ import io.mrnateriver.smsproxy.models.MessageProxyRequest
 import io.mrnateriver.smsproxy.relay.services.settings.SettingsService
 import io.mrnateriver.smsproxy.shared.models.MessageEntry
 import kotlinx.coroutines.FlowPreview
-import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.timeout
 import javax.inject.Inject
-import kotlin.time.Duration
+import kotlin.time.Duration.Companion.seconds
 import io.mrnateriver.smsproxy.shared.contracts.MessageRelayService as MessageRelayServiceContract
+
+private const val RECEIVER_KEY_TIMEOUT_SECONDS = 1
 
 class MessageRelayService @Inject constructor(
     private val proxyApiService: DefaultApi,
@@ -20,21 +22,23 @@ class MessageRelayService @Inject constructor(
 
     @OptIn(FlowPreview::class)
     override suspend fun relay(entry: MessageEntry) {
-        settingsService.receiverKey.timeout(Duration.ZERO).collectLatest { receiverKey ->
-            val response = proxyApiService.messagesProxy(
-                MessageProxyRequest(
-                    receiverKey,
-                    entry.messageData.sender,
-                    entry.messageData.message,
-                    entry.messageData.receivedAt,
-                )
+        val receiverKey =
+            settingsService.receiverKey.timeout(RECEIVER_KEY_TIMEOUT_SECONDS.seconds).first()
+
+        val response = proxyApiService.messagesProxy(
+            MessageProxyRequest(
+                receiverKey,
+                entry.messageData.sender,
+                entry.messageData.message,
+                entry.messageData.receivedAt,
             )
-            if (!response.isSuccessful) {
-                val body = response.errorBody()?.string()
-                throw IllegalStateException(
-                    "Failed to relay message, status code: ${response.code()} ${response.message()}${if (body.isNullOrBlank()) "" else "\n$body"}"
-                )
-            }
+        )
+
+        if (!response.isSuccessful) {
+            val body = response.errorBody()?.string()
+            throw IllegalStateException(
+                "Failed to relay message, status code: ${response.code()} ${response.message()}${if (body.isNullOrBlank()) "" else "\n$body"}"
+            )
         }
     }
 
