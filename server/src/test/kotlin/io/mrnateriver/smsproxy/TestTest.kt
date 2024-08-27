1771d48ca01629246a0b6dc593dc725fed0ac622
Evgenii Dobrovidov <mr.nate.river@gmail.com>
2024-08-27 21:56:37 +0200

    chore: add a test for testing out tests in server so that we can test while we test

---
+++ server/src/test/kotlin/io/mrnateriver/smsproxy/TestTest.kt
@@ -0,0 +1,11 @@
+package io.mrnateriver.smsproxy
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class TestTest {
+    @Test
+    fun `math should work`() {
+        assertEquals(4, 2 + 2)
+    }
+}
