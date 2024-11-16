becd6995352adccd814198330260d81f90c6e999
Evgenii Dobrovidov <mr.nate.river@gmail.com>
2024-11-16 16:39:11 +0100

    chore: minor updates to Gradle build configs
    In particular, silences Gradle warning about JDK version mismatch in buildSrc

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -6,6 +6,15 @@ repositories {
     mavenCentral()
 }
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
 dependencies {
     compileOnly(libs.kotlin.gradle.plugin)
     implementation(libs.ktor.network.tls.certificates)
