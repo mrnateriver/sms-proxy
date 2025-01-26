1ed5ddbed3e56cff010f2d97e0b6a6acf6c556c8
Evgenii Dobrovidov <mr.nate.river@gmail.com>
2025-01-26 20:08:01 +0100

    chore(server): dummy change to test pipeline trigger
    (cherry picked from commit e1419f6d0a87f30f36cce7f0edcf550445699f60)

--- server/src/main/kotlin/io/mrnateriver/smsproxy/server/Api.kt
+++ server/src/main/kotlin/io/mrnateriver/smsproxy/server/Api.kt
@@ -11,6 +11,8 @@ import io.mrnateriver.smsproxy.controllers.ReceiversController.Companion.receive
 import io.mrnateriver.smsproxy.controllers.ReceiversRegisterController.Companion.receiversRegisterRoutes
 import io.mrnateriver.smsproxy.server.framework.DaggerFramework
 
+// test change
+
 fun Application.installApi(serverConfig: ServerConfiguration, telemetryServices: TelemetryServices) {
     install(ContentNegotiation) {
         json()
