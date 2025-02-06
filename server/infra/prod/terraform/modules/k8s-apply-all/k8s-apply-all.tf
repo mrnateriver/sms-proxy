a08f42ca36435e72019d3b152744f8ba87c0c1f5
Evgenii Dobrovidov <mr.nate.river@gmail.com>
2025-02-06 23:12:01 +0100

    infra(server): fix and slightly refactor k8s bulk apply terraform module

--- server/infra/prod/terraform/modules/k8s-apply-all/k8s-apply-all.tf
+++ server/infra/prod/terraform/modules/k8s-apply-all/k8s-apply-all.tf
@@ -1,3 +1,21 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.35.1"
+    }
+  }
+}
+
+provider "kubernetes" {
+  config_context = var.context
+}
+
+variable "context" {
+  type        = string
+  description = "Name of the K8S context in your ~/.kube/config file that will be used for provisioning"
+}
+
 variable "dirname" {
   type        = string
   description = "Directory name of the K8S resources to be applied"
@@ -9,19 +27,25 @@ variable "namespace" {
   default     = "sms-proxy"
 }
 
+locals {
+  k8s_dir = "${path.module}/../../../k8s/${var.dirname}"
+  manifests = {
+    for file in fileset(local.k8s_dir, "*.yml")
+    : file => yamldecode(file("${local.k8s_dir}/${file}"))
+  }
+}
+
 resource "kubernetes_manifest" "k8s_manifests" {
-  for_each = fileset("${path.module}/../../../k8s/${var.dirname}", "*.yml")
+  for_each = local.manifests
   manifest = merge(
-    yamldecode(file("${path.module}/../../../k8s/${var.dirname}/${each.value}")),
-    {
-      metadata = {
-        namespace = var.namespace
-      }
-    }
+    each.value,
+    contains(keys(each.value.metadata), "namespace") ?
+    { metadata = merge(each.value.metadata, { namespace = var.namespace }) } :
+    {}
   )
 
   lifecycle {
-    ignore_changes = [metadata]
+    ignore_changes = [manifest.metadata]
   }
 
   wait {
